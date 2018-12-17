3499e1d52ac8de09370d4841a01d03f982ab2cbc
Terri Bedore <terribedore@gmail.com>
2018-12-17 12:07:35 -0600

    removed the original curly brackets around the search wrapper

--- src/Search.js
+++ src/Search.js
@@ -44,14 +44,14 @@ return (
     <div className="search-books-bar">
       <Link className="close-search" to="/">Close</Link>
         <div className="search-books-input-wrapper">
-          {/*
+          /*
           NOTES: The search from BooksAPI is limited to a particular set of search terms.
           You can find these search terms here:
           https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
           However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
           you don't find a specific author or title. Every search is limited by search terms.
-          */}
+          */
           <input type="text" placeholder="Search by title or author"/>
         </div>
       </div>
@@ -59,10 +59,6 @@ return (
         <ol className="books-grid"></ol>
       </div>
   </div>
-  ) : (
-  <div className="list-books">
-    <Link className="open-search" to="/">Add a book</Link>
-  </div>}
   )
 }
 

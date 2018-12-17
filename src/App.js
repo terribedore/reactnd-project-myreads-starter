72c32593d6ad77919ab7d3d52a785f0c35896226
Terri Bedore <terribedore@gmail.com>
2018-12-17 11:41:10 -0600

    Pulled some of the search starter code into own component.

--- src/App.js
+++ src/App.js
@@ -47,45 +47,11 @@ class BooksApp extends React.Component {
         )}/>
         //link to 'search page'
         <Route path="/search" render={({ history }) => (
-          // NOTE: add the search stuff here!
+          <Search
+          onShelfChange={this.onShelfChange}
+          history={history}
+          books={this.state.books}/>
         )}/>
-      </div>
-    )
-  }
-}
-
-// TODO: rework this as most is above. Potentially add all the info about the books into their OWN Component??
-  render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
       </div>
     )
   }
---
+++ src/Search.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
+
+class Search extends React.Component {
+  state = {
+    searchResults: []
+  }
+
+  //Keeps track of the input value
+  search = (e) => {
+    const query = e.target.value;
+    if (!query) {
+      this.setState({searchResults: []});
+      return;
+    }
+
+  //Call to the search API
+  BooksAPI
+    .search(query, 20)
+    .then(searchResults => {
+      if (!searchResults || searchResults.error) {
+        this.setState({searchResults: []});
+        return;
+      }
+      // map over the books returned from the search API, and check if they are on the shelf or not
+      searchResults = searchResults.map((book) => {
+        const bookOnShelf = this.props.books.find(b => b.id === book.id);
+        book.shelf = bookOnShelf
+        ? bookOnShelf.shelf
+        : "none";
+        return book;
+      });
+
+    this.setState({searchResults});
+
+    });
+  };
+
+render() {
+return (
+  <div className="search-books">
+    <div className="search-books-bar">
+      <Link className="close-search" to="/">Close</Link>
+        <div className="search-books-input-wrapper">
+          {/*
+          NOTES: The search from BooksAPI is limited to a particular set of search terms.
+          You can find these search terms here:
+          https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+          However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+          you don't find a specific author or title. Every search is limited by search terms.
+          */}
+          <input type="text" placeholder="Search by title or author"/>
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid"></ol>
+      </div>
+  </div>
+  ) : (
+  <div className="list-books">
+    <Link className="open-search" to="/">Add a book</Link>
+  </div>}
+  )
+}
+
+export default Search

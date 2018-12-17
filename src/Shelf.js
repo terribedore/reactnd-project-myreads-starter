5589081d2fc7de98b3ec626979ef101a63828b66
Terri Bedore <terribedore@gmail.com>
2018-12-17 12:06:38 -0600

    trying to get the book categories sorted out.

---
+++ src/Shelf.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Book from './Book'
+
+function Shelf(props) {
+
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.bookshelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.bookshelfBooks.map((book) => {
+            return <li key={book.id}>
+              <Book book={book}
+              onShelfChange={props.onShelfChange}/>
+            </li>
+          })}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Shelf
